81a9ab747206f9db37a1459c727476cbfcf33800
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-03-27 15:05:33 +0100

    Update dependencies and paths after directory restructure
    - There were odds and ends i needed to fix which i forgot when i
      rearranged the directory strucure

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,16 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
+const ENV = process.env.NODE_ENV || "development";
+
+const baseEnvPath = `${__dirname}/../../.env`;
+dotenv.config({ path: baseEnvPath });
+dotenv.config({
+  path: `${baseEnvPath}.${ENV}`,
 });
 
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE not set");
 }
 
 module.exports = new Pool();
--- src/db/seeds/run-seed.js
+++ src/db/seeds/run-seed.js
@@ -1,11 +1,14 @@
-const devData = require("../data/development-data/index.js");
+const devData = require("../data/development-data");
+const testData = require("../data/test-data");
 const seed = require("./seed.js");
 const db = require("../");
 
-export default function runSeed() {
+function runSeed() {
   return seed(process.env.NODE_ENV ? testData : devData);
 }
 
 if (require.main === module) {
   runSeed().then(() => db.end());
 }
+
+module.exports = runSeed;
