bf7e386c990cafc7279c7679c3d1a0f98f4aea4e
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-04-01 00:49:48 +0100

    You actually wouldn't beliewve why the tests were failing:
    - Basically i forgot to change the files to exclude from the tsc and it
      was double testing files in both ts && js

--- src/__tests__/commentsRoutes.test.ts
+++ src/__tests__/commentsRoutes.test.ts
@@ -15,9 +15,6 @@ afterAll(() => {
 // deleted.
 describe("DELETE /api/comments/:commentId", () => {
   describe("Happy path", () => {
-    it("should return a 204", async () => {
-      await request(app).delete("/api/comments/8").expect(204);
-    });
     it("should not return any conetnt", async () => {
       const { body } = await request(app).delete("/api/comments/2").expect(204);
       expect(body).toEqual({});
